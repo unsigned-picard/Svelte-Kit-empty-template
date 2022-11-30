8941c9ec92f1329c9f499783dd216546ea375380
Hugo <picard.hugo08@gmail.com>
2022-11-30 19:59:28 +0100

    init

---
+++ src/routes/branch/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+    export let data;
+
+    console.log(data.props.product)
+</script>
+
+
+<div class="w-full mx-32 flex flex-row">
+    <div class="flex grid grid-cols-1">
+        <div class="h-64 w-64 bg-contain bg-no-repeat mr-8 mb-8"
+             style="background-image: url({data.props.product.image})"></div>
+        </div>
+    </div>
+    <div class="flex ml-8">
+        <div class="w-64">
+            <h2 class="font-bold text-lg mb-16">{data.props.product.title}</h2>
+            <div class="text-sm">{data.props.product.description}</div>
+        </div>
+    </div>
